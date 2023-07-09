453d777ae7a7119809b76d8bc9064131a72c47a3
t-e-r-m <newenewen@tutanota.com>
2023-07-09 14:06:54 +0530

    Set magit-log date format to "%y%m%d %H%M%S"

--- packages/magit.el
+++ packages/magit.el
@@ -3,4 +3,5 @@
   :preface
   (setq magit-auto-revert-mode nil
 	magit-diff-refine-hunk t
-        magit-save-repository-buffers nil))
+        magit-save-repository-buffers nil
+        magit-log-margin '(t "%y%m%d %H%M%S" magit-log-margin-width t 15)))
