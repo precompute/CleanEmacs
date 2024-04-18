2c671bb1afc152c719b22b79152bf6ce76ff714f
precompute <git@precompute.net>
2024-04-18 21:10:43 +0530

    Configure perfect-margin to ignore certain buffers.

--- packages/perfect-margin.el
+++ packages/perfect-margin.el
@@ -6,5 +6,8 @@
           magit-mode) . perfect-margin-mode)
   :config
   (setq-default perfect-margin-lighter nil
-                perfect-margin-visible-width 130)
+                perfect-margin-visible-width 130
+                perfect-margin-ignore-filters (window-minibuffer-p) ;; does nothing much
+                perfect-margin-ignore-regexps (list "^[[:space:]]*\\*m" ;; gets rid of most minibuffers, especially vertico
+                                                    "^[[:space:]]*\\*transient"))
   (add-hook 'org-mode-hook #'(lambda () (interactive) (setq-local perfect-margin-visible-width 80))))
