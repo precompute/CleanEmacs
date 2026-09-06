6aa46990b9e5619d30e8503f79dd78c413cf8462
precompute <git@precompute.net>
2026-09-07 03:33:05 +0530

    More completion-preview configuration changes

--- packages/inbuilt/completion-preview.el
+++ packages/inbuilt/completion-preview.el
@@ -1,7 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 (use-package completion-preview
   :ensure nil
-  :hook (prog-mode . completion-preview-mode)
+  :hook ((eglot-managed-mode
+          emacs-lisp-mode) . completion-preview-mode)
   :bind ( :map completion-preview-active-mode-map
           ("<tab>" . completion-preview-complete)
           ("M-<tab>" . completion-preview-insert)
@@ -9,5 +10,6 @@
           ("M-p" . completion-preview-prev-candidate))
   :config
   (setq completion-preview-minimum-symbol-length 2
-        completion-preview-idle-delay 0.3
-        completion-preview-message-format nil))
+        completion-preview-idle-delay 0.5
+        completion-preview-message-format nil
+        completion-preview-adapt-background-color nil))
